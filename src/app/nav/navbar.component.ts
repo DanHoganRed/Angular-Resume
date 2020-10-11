4ef4e9a1d0da2fe6e940c1aac740cb35b3b284e5
danhoganred <irishvking@hotmail.com>
2020-10-10 19:31:15 -0600

    Cleaned up nabvbar

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -3,34 +3,8 @@ import { SlideService } from "../slide.service";
 
 @Component({
     selector: 'nav-bar',
-    template: `
-    <nav class="navbar navbar-default border" style="background:#4c849985">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <a class="navbar-brand" href="#">Dan Hogan</a>
-      </div>
-      <ul class="nav nav-pills">
-        <li class="nav-item" (click)='slider.setCurrentSlide(0)'><button class="nav-link" [ngClass]="slider.getCurrentSlide() === 0 ? 'active':'' ">Bio</button></li>
-        <li class="nav-item" (click)='slider.setCurrentSlide(1)'><button class="nav-link" [ngClass]="slider.getCurrentSlide() === 1 ? 'active':'' ">Skills</button></li>
-      </ul>
-    </div>
-  </nav>
-    `,
-    styles:[`
-    .navbar-default {
-      
-    }
-    button.nav-link { 
-      background:none;
-      border:none; 
-    }
-    .navbar-brand {
-      color:white
-    }
-    .nav-link.active {
-      background-color:#a7b1b1b8
-    }
-    `]
+    templateUrl: './navbar.component.html',
+    styleUrls:['./navbar.component.scss']
     
   })
   /**
